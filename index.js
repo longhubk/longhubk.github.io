08f1a2387d72d06d02868c01693c6bcd8339e68e
Nguyen Thanh Long <longn7284@gmail.com>
2021-11-01 02:55:58 +0700

    add pagination and private public

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // console.log('index.js start');
-const server_url = "https://dblogit.herokuapp.com";
+// const server_url = "https://dblogit.herokuapp.com";
+const server_url = "http://localhost:3000";
 
 const checkIsLogin = async () => {
   const oldTK = localStorage.getItem('token');
@@ -28,7 +29,6 @@ const taoxinmay = () => {
 const logout = () => {
   const oldTK = localStorage.getItem('token');
   console.log('logout', oldTK);
-  console.log('cho tao xin may bug loi ra di', oldTK);
   localStorage.removeItem('token');
   location.reload();
 }
@@ -85,11 +85,26 @@ const renderNote = (rawMD) => {
   const res = md.render(rawMD);
   return res;
 }
+const getAdminListNote = async (page = 1) => {
+  try {
+    const oldTK = localStorage.getItem('token') 
+    let data = await axios.get(`${server_url}/note/page-ad/${page}`, { headers: { Authorization: `Bearer ${oldTK}` }} );
+    if (data.data.code === '00') {
+      const res = data.data.msg;
+      // console.log('res note', data.data.msg.content);
+      return res;
+    } else {
+      console.log('not note');
+      return false;
+    }
+  } catch (err) { console.log(err) };
+}
 
-const getListNote = async () => {
+
+const getListNote = async (page = 1) => {
   // console.log('getListNote');
   try {
-    const data = await axios.get(`${server_url}/note`);
+    const data = await axios.get(`${server_url}/note/page/${page}`);
     // console.log('data', data);
     if (data.data.code === '00') {
       const res = data.data.msg;
@@ -102,10 +117,11 @@ const getListNote = async () => {
   } catch (err) { console.log(err) };
 }
 
-const getNote = async (noteId) => {
+const getAdminNote = async (noteId) => {
   // console.log('getNote', noteId);
   try {
-    const data = await axios.get(`${server_url}/note/${noteId}`);
+    const oldTK = localStorage.getItem('token') 
+    const data = await axios.get(`${server_url}/note/one-ad/${noteId}`, { headers: { Authorization: `Bearer ${oldTK}` }});
     if (data.data.code === '00') {
       const res = data.data.msg;
       return res;
@@ -116,17 +132,62 @@ const getNote = async (noteId) => {
   } catch (err) { console.log(err) };
 }
 
-const updateNote = async (noteId, title, content) => {
-  const oldTK = localStorage.getItem('token');
+const getNote = async (noteId) => {
+  // console.log('getNote', noteId);
+  try {
+    const data = await axios.get(`${server_url}/note/one/${noteId}`);
+    if (data.data.code === '00') {
+      const res = data.data.msg;
+      return res;
+    } else {
+      console.log('not note');
+      return false;
+    }
+  } catch (err) { console.log(err) };
+}
+
+const getCountNote = async () => {
+  // console.log('getNote', noteId);
+  try {
+    const data = await axios.get(`${server_url}/note/count`);
+    if (data.data.code === '00') {
+      const res = data.data.msg;
+      return res;
+    } else {
+      console.log('not note');
+      return false;
+    }
+  } catch (err) { console.log(err) };
+}
+
+const getAdminCountNote = async () => {
+  // console.log('getNote', noteId);
+  try {
+    const oldTK = localStorage.getItem('token') 
+    const data = await axios.get(`${server_url}/note/count-ad`, { headers: { Authorization: `Bearer ${oldTK}` } });
+    if (data.data.code === '00') {
+      const res = data.data.msg;
+      return res;
+    } else {
+      console.log('not note');
+      return false;
+    }
+  } catch (err) { console.log(err) };
+}
+
+
+
+const updateNote = async (updateData, oldData) => {
+  const oldTK = localStorage.getItem('token') 
   if (oldTK) {
     try {
-      const data = await axios.post(`${server_url}/note/update`, { noteId, title, content }, { headers: { Authorization: `Bearer ${oldTK}` } });
+      const data = await axios.post(`${server_url}/note/update`, {updateData, oldData}, { headers: { Authorization: `Bearer ${oldTK}` } });
       // console.log('update res', data);
       if (data.data.code === '00') {
         console.log('update ok', JSON.stringify(data.data.msg));
         return data.data;
       } else {
-        console.log('update fail');
+        console.log('update fail', JSON.stringify(data.data));
         return data.data;
       }
     } catch (err) { console.log(err) };
