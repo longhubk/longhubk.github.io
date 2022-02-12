63f8293b680cf5f128c77d1db05cae8493e3668a
long <longn7284@gmail.com>
2022-02-12 13:43:33 +0700

    add countdown feature

---
+++ countdown.js
@@ -0,0 +1,32 @@
+var cd;
+const setCountdown = (countDownDate, elm) => {
+  cd = setInterval(function () {
+    // Get today's date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    var dt = new Date(countDownDate);
+
+    // Output the result in an element with id="demo"
+    elm.innerHTML =
+      `<p>Premiere on <b>${dt.toLocaleDateString('vi-VN')}</b> at <b>${dt.toLocaleTimeString('vi-VN')}</b></p><p>Chờ thêm <b>${days} ngày ${hours} giờ ${minutes} phút ${seconds} giây</b> nữa nhé <3</p>`;
+
+    // If the count down is over, write some text
+    if (distance < 0) {
+      clearInterval(cd);
+      // elm.innerHTML = "";
+      console.log('EXPIRED');
+      window.location.reload();
+    }
+  }, 1000);
+};
